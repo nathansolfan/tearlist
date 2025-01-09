2c8b269d38d456a862f952ccd4e3556a1f0c1d4a
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-09 23:49:39 +0000

    updated livewire component

--- app/Livewire/TierList.php
+++ app/Livewire/TierList.php
@@ -6,6 +6,24 @@ use Livewire\Component;
 
 class TierList extends Component
 {
+
+    public $tasks;
+
+    public function mount($tasks)
+    {
+        $this->tasks = $tasks;
+    }
+
+    public function calculateProgress($created_at, $deadline)
+    {
+        $totalTime = strtotime($deadline) - strtotime($created_at) ;
+        $elapsedTime = time() - strtotime($created_at);
+
+        return min(($elapsedTime / $totalTime) * 100,100);
+    }
+
+
+
     public function render()
     {
         return view('livewire.tier-list');
