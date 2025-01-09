ef219cca49f6e01ecb4091932e4baf0865412c36
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-09 23:45:59 +0000

    created a livewire component

---
+++ app/Livewire/TierList.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class TierList extends Component
+{
+    public function render()
+    {
+        return view('livewire.tier-list');
+    }
+}
