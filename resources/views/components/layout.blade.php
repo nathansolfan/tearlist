3dff21c056f96267f4c53e4fada40baf1f2ac066
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-24 19:17:02 +0000

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name', 'Tier List To-Do App') }}</title>
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Livewire Styles -->
+    @livewireStyles
 </head>
 <body>
     <div>
