c43b4e7da4fbd7f0c365ed1789e66ebf5a9272df
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-28 22:52:50 +0000

    update

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,16 +15,24 @@ class TaskController extends Controller
 
     public function create()
     {
-        $tiers = ['s','a','b','c','d','e'];
-        return view('tasks.register', compact('tiers'));
+        // $tiers = ['s','a','b','c','d','e'];
+        // return view('tasks.register', compact('tiers'));
+        return view('tasks.form');
     }
 
     public function store(Request $request)
     {
         // validate
         $validated = $request->validate([
-
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:1000',
+            'tier' => 'required|in:s,a,b,c,d,e',
+            'deadline' => '',
         ]);
 
+        // create
+        Task::create($validated);
+
+        return redirect('/')->with('success', 'Task created successfully!');
     }
 }
