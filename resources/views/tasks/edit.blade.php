eb872e81257af0f57bedbba75a43b1cdd9f56af0
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-30 14:55:59 +0000

    update

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -12,6 +12,24 @@
             </ul>
         </div>
         @endif
+        {{-- Task Form --}}
+        <form action="tasks.update" method="POST">
+            @csrf
+            @method('PUT')
+            <div>
+                <label for="title">Title</label>
+                <input type="text" name="title" id="title" value=" {{old('title', $task->title)}} " required>
+            </div>
+
+            {{-- description --}}
+            <div>
+                <label for="description">Description</label>
+                <textarea name="description" id="description" rows="4" required> {{old('description', $task->description)}} </textarea>
+
+            </div>
+
+        </form>
+
 
     </div>
 </x-layout>
