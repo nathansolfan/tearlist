39eff71bee5d7d3d3d305f0bf2a7797d6e62920f
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2025-01-31 19:03:47 +0000

    update

--- resources/views/components/task-form.blade.php
+++ resources/views/components/task-form.blade.php
@@ -5,4 +5,13 @@ class="space-y-6">
 @csrf
 @if ($isEdit) @method('PUT') @endif
 
+{{-- title --}}
+<div>
+    <label for=""></label>
+    <input type="text" name="" id=""
+    value=" {{old('title', $task->tite ?? '')}} "
+    class=""
+    required>
+</div>
+
 </form>
